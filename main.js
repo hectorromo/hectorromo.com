303d09e2d8b92457852f716e96e0fc9fd946cd70
Hector Romo <info@hectorromo.com>
2016-07-02 20:45:15 +0200

    First real commit

---
+++ main.js
@@ -0,0 +1,34 @@
+// var canvidControl = canvid({
+//     selector : '.video',
+//     videos: {
+//         clip1: { src: 'video/idear.png', frames: 74, cols: 10, fps: 10, loops: 1, onEnd: function(){
+//           console.log('clip1 ended.');
+//           // canvidControl.play('clip2');
+//         }}
+//         // clip2: { src: 'clip2.jpg', frames: 43, cols: 6, fps: 24 }
+//     },
+//     width: 320,
+//     height: 250,
+//     loaded: function() {
+//         canvidControl.play('clip1');
+//         // reverse playback
+//         // canvidControl.play('clip1', true);
+//     }
+// });
+
+
+
+
+
+// var images = 75;
+// var images = document.getElementById('images');
+
+// var img = document.createElement('img');
+// img.src = './video/idearideas/IMG_0001.jpg';
+// for (var i = 0; i < 75; i++) {
+//     var img = document.createElement('img');
+//     img.src = './video/idearideas/IMG_00' + i + '.jpg';
+
+//     images.appendChild(img);
+
+// }
